2e3198950c75edcc1f2b71b2b1c50825d6680e2b
dddk1ddd <121657626+dddk1ddd@users.noreply.github.com>
2023-01-04 04:07:59 -0500

    Add files via upload

---
+++ j0nez-011c/README.txt
@@ -0,0 +1,56 @@
+# j0nez v.011c
+OpenAI ChatGPT Discord Bot
+www.phatkid.art
+
+Responds to mentions with normal conversation through ChatGPT.
+Generates AI Images in chat from prompts using DALL-E.
+Morphs images into specified number of iterations.
+Combines an image and an image mask to create new images from prompt.
+Minimalist Help system.
+
+Getting Started:
+Create a file called '.env' in the folder with j0nez.py.
+The file should only contain these two lines:
+
+TOKEN='YOUR_BOT_TOKEN'
+KEY='YOUR_API_KEY'
+
+Replace YOUR_BOT_TOKEN with your Discord Bot Token.
+Replace YOUR_API_KEY with your OpenAI API Key.
+
+Both can be obtained from their respective developer portals.
+Or, you can add j0nez via this link:
+https://discord.com/api/oauth2/authorize?client_id=1058807251667787827&permissions=534723951680&scope=bot%20applications.commands
+
+Usage:
+Commands start with '!'.
+Images should be square and less than 4mb.
+
+!image <description>
+Generates a 512x512 image from a prompt.
+
+!morph <number>
+Morphs an image up to 10 times.
+Works best with images generated with !image.
+
+!remix <prompt> <image> <mask>
+prompt
+A text description of the desired images.
+image
+The image to edit. Must be a valid PNG file,
+less than 4MB, and square. Works best with images from !image.
+mask
+An additional image whose fully transparent areas indicate
+where image should be edited. Must be a valid PNG file,
+less than 4MB, and have the same dimensions as image.
+Included in the archive is mask.png for testing.
+
+From ChatGPT:
+"Phat Kid is an experimental electronic music artist who has a diverse 
+and eclectic style that incorporates elements of various genres, including 
+hip hop, techno, and ambient music. His music is characterized by its experimental 
+and unconventional approach, and he often uses unconventional instruments and production 
+techniques to create unique and experimental sounds. Phat Kid's music is highly 
+experimental and often incorporates elements of improvisation and chance, making 
+each of his performances and recordings unique. His music is often described as 
+being abstract and avant-garde, and it explores a wide range of themes and emotions."
---
+++ j0nez-011c/j0nez.py
@@ -0,0 +1,182 @@
+import discord
+import openai
+import requests
+import os
+import time
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# j0nez ChatGPT bot v.011c by PK
+# www.phatkid.art
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+load_dotenv()
+
+token = os.getenv('TOKEN')
+apikey = str("Bearer " + str(os.getenv('KEY')))
+openai.api_key = str(os.getenv('KEY'))
+client = commands.Bot(
+    command_prefix='!',
+    self_bot=False,
+    intents=intents
+)
+
+
+class MyHelpCommand(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        e = discord.Embed(color=discord.Color.blurple(), description='')
+        for page in self.paginator.pages:
+            e.description += page
+        await destination.send(embed=e)
+
+
+client.help_command = MyHelpCommand()
+
+
+@client.command(
+    name="version",
+    help="j0nez v.011c by PK - www.phatkid.art"
+)
+async def version(ctx):
+    await ctx.send("j0nez v.011c by PK - www.phatkid.art")
+
+
+@client.command(
+    name="ping",
+    help="keepalive"
+)
+async def ping(ctx):
+    await ctx.send("Pong! Somewhere right now it is " + str(time.asctime()))
+
+
+@client.command(
+    name="image",
+    help="""
+    AI generated image from a description.
+    The more descriptive, the better.
+    """
+)
+async def image(ctx, *, description):
+    prompt = ' '.join(description)
+    try:
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="512x512"
+        )
+        output = response['data'][0]['url']
+        await ctx.send(output)
+    except openai.error.OpenAIError as e:
+        await ctx.send("Error Code: " + str(e.http_status))
+        await ctx.send(e.error)
+
+
+@client.command(
+    name="remix",
+    help="""
+    !remix <prompt> <image> <mask>
+    prompt
+    A text description of the desired image.
+    image
+    The image to edit. Must be a valid PNG file,
+    less than 4MB, and square. Works best with !image.
+    mask
+    An additional image whose fully transparent areas indicate
+    where image should be edited. Must be a valid PNG file,
+    less than 4MB, and have the same dimensions as image.
+    """
+)
+async def remix(ctx):
+    prompt = ctx.message.content
+    try:
+        image = ctx.message.attachments[0]
+        image_data = await image.read()
+        mask = ctx.message.attachments[1]
+        mask_data = await mask.read()
+    except:
+        await ctx.send("You must provide two square PNG files less than 4MB each.")
+    try:
+        response = openai.Image.create_edit(
+            image_data,
+            mask_data,
+            prompt=prompt,
+            n=1,
+            size="512x512"
+        )
+        output = response['data'][0]['url']
+        await ctx.send(output)
+    except openai.error.OpenAIError as e:
+        await ctx.send("Error Code: " + str(e.http_status))
+        await ctx.send(e.error)
+
+
+
+@client.command(
+    name="morph",
+    help="""
+    Morphs an image [number] of times,
+    with a maximum of 10.
+    Must be a square PNG less than 4MB.
+    Works best with images from !image.
+    """
+)
+async def morph(ctx, number=1):
+    image = ctx.message.attachments[0]
+    image_data = await image.read()
+    try:
+        response = openai.Image.create_variation(
+            image_data,
+            n=number,
+            size="512x512"
+        )
+        for i in range(int(number)):
+            output = response['data'][i]['url']
+            await ctx.send(output)
+    except openai.error.OpenAIError as e:
+        await ctx.send("Error Code: " + str(e.http_status))
+        await ctx.send(e.error)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    # Ignore own self
+    if message.author == client.user:
+        return
+    # Process bot commands
+    await client.process_commands(message)
+    if message.content.startswith("!"):
+        return
+    # I was mentioned
+    if client.user in message.mentions:
+        # Send a message in response to the mention
+        prompt = message.content[len(client.user.mention) + 1:]
+        url = "https://api.openai.com/v1/completions"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": apikey,
+        }
+        data = {
+            "model": "text-davinci-003",
+            "prompt": prompt,
+            "max_tokens": 4000,
+            "temperature": 1,
+            "presence_penalty": -2,
+            "frequency_penalty": 2
+        }
+        response = requests.post(url, headers=headers, json=data)
+        output = response.json()['choices'][0]['text']
+        await message.channel.send(output)
+        return
+
+client.run(token)
